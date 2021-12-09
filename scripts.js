6d52d09e2860842bc43b711087fee0f0f288a02d
Gabriel Bastos <gbastosbarbosa@gmail.com>
2021-12-09 19:52:44 -0300

    loop's

--- scripts.js
+++ scripts.js
@@ -1,20 +1,40 @@
-//1.for 
+// //1.for 
 
-//apresentando numeros Pares
-for (let i = 0; i < 5; i++) {
-    console.log('Uma frase ou um objeto a ser repetido', i);
-}
+// //apresentando uma frase
+// for (let i = 0; i < 5; i++) {
+//     console.log('Uma frase ou um objeto a ser repetido', i);
+// }
 
-//apresentando numeros impares
-for (let i = 1; i < 100; i++) {
-    if (i % 2 !== 0) {
-        console.log(i);
-    }
-}
+// //apresentando numeros impares
+// for (let i = 1; i < 100; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
 
-//apresentando numeros Pares
-for (let i = 1; i < 100; i++) {
-    if (i % 2 !== 1) {
-        console.log(i);
-    }
-}
+// //apresentando numeros Pares
+// for (let i = 1; i < 100; i++) {
+//     if (i % 2 !== 1) {
+//         console.log(i);
+//     }
+// }
+
+// //while loop 
+// //apresentando numeros impares
+// let i = 5;
+
+// while (i >= 1) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+//     i--;
+// }
+
+//do.. while 
+//apresentando uma frase
+let i = 0;
+
+do {
+    console.log('Digitando...', i);
+    i++;
+} while (i < 10)
