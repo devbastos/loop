80df1019d99a3737e484d0a32a1e0c20db7bd961
Gabriel Bastos <gbastosbarbosa@gmail.com>
2021-12-09 18:59:31 -0300

    loop

---
+++ scripts.js
@@ -0,0 +1,20 @@
+//1.for 
+
+//apresentando numeros Pares
+for (let i = 0; i < 5; i++) {
+    console.log('Uma frase ou um obj a ser repetido', i);
+}
+
+//apresentando numeros impares
+for (let i = 1; i < 100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+//apresentando numeros Pares
+for (let i = 1; i < 100; i++) {
+    if (i % 2 !== 1) {
+        console.log(i);
+    }
+}
